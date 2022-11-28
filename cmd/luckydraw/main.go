502cfc9c27231e5feb0a2e19c6197133d6fc8a59
Sungup Moon <sungup.moon@samsung.com>
2022-11-28 17:52:12 +0900

    cmd.luckydraw: building layout (WIP)
    Signed-off-by: Sungup Moon <sungup@me.com>

--- cmd/luckydraw/main.go
+++ cmd/luckydraw/main.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	partMembers := NewMembers("All Members", "Sungup", "Lucy", "Seonho", "Dan", "Changduk")
+	top1 := NewMembers("Top 1", "hello")
+	top2 := NewMembers("Top 2")
+	top3 := NewMembers("Top 3")
+	top4 := NewMembers("Top 4")
+	top5 := NewMembers("Top 5")
+	top6 := NewMembers("Top 6")
+	top7 := NewMembers("Top 7")
+
+	hello := widget.NewLabel("Helly Fyne!")
+	button := widget.NewButton(
+		"Hi!",
+		func() {
+			hello.SetText("Welcome :)")
+		},
+	)
+
+	body := container.NewGridWithColumns(4,
+		container.NewGridWithRows(
+			3,
+			top1.Widget(200, 600),
+			top2.Widget(200, 600),
+			top3.Widget(200, 600),
+		),
+		container.NewGridWithRows(
+			2,
+			top4.Widget(200, 600),
+			top5.Widget(200, 600),
+		),
+		top6.Widget(200, 600),
+		top7.Widget(200, 600),
+	)
+
+	left := partMembers.Widget(200, 600)
+
+	dummy := container.NewWithoutLayout()
+	dummy.Resize(fyne.NewSize(1, 1))
+	doc := container.NewBorder(hello, button, left, body)
+	doc.Add(hello)
+	doc.Add(button)
+	doc.Add(body)
+	doc.Add(left)
+
+	container.NewVBox()
+
+	w.SetContent(doc)
+	w.Resize(fyne.Size{Width: 1024, Height: 768})
+
+	w.ShowAndRun()
+
+}
--- cmd/luckydraw/members.go
+++ cmd/luckydraw/members.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -23,8 +24,18 @@ func (m *Members) UpdateItem(i widget.ListItemID, o fyne.CanvasObject) {
 	o.(*widget.Label).SetText(m.members[i])
 }
 
-func (m *Members) Widget() *widget.List {
-	return m.widget
+func (m *Members) Widget(width, height float32) fyne.CanvasObject {
+	c := container.NewWithoutLayout()
+
+	label := widget.NewLabel(m.title)
+	label.Resize(fyne.NewSize(width, 32.0))
+	m.widget.Resize(fyne.NewSize(width, height))
+	m.widget.Move(fyne.NewPos(0.0, 32.0))
+	c.Add(label)
+	c.Add(m.widget)
+
+	c.Resize(fyne.NewSize(width, height))
+	return c
 }
 
 func NewMembers(title string, members ...string) *Members {
